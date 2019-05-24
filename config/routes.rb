6f3d1e907b574205f372da4263dd9e4aa2897aef
alex <alcebaeza@gmail.com>
2019-05-24 06:48:28 -0400

    agregado root pages#one en routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'dashboards/one'
   get 'dashboards/two'
+  root 'pages#one'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
